36dc89c74aa4949097c86c0fb39ad79b047e5c76
Bell, Ryan <ryan.bell62101@gmail.com>
2023-07-05 20:52:46 -0400

    fixed linting issues

--- service/routes.py
+++ service/routes.py
@@ -57,25 +57,27 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+    """
+    Lists all Accounts
+    This endpoint will list all Accounts stored by the service
+    """
     app.logger.info("Request to list all Accounts")
-
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
+
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -84,15 +86,14 @@ def read_account(account_id):
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
-    if account == None:
+    if not account:
         return make_response(status=status.HTTP_404_NOT_FOUND)
     return make_response(account.serialize(), status.HTTP_200_OK)
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-# ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
@@ -104,11 +105,10 @@ def update_account(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-# ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     app.logger.info("Request to read an Account with id %d", account_id)
@@ -121,8 +121,6 @@ def delete_account(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
